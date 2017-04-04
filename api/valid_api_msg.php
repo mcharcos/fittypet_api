c69c3a080bd4f819f31a9199156af14abae59bad
mcharcos <mvcharcos@gmail.com>
2017-04-04 10:51:33 +0100

    Updated valid api message page

--- api/valid_api_msg.php
+++ api/valid_api_msg.php
@@ -1,7 +1,11 @@
 
 <ul>
-    <li> api/v1/category/list </li>
-    <li> api/v1/category/details/[category_id] </li>
-    <li> api/v1/category/books/[category_id] </li>
-    <li> api/v1/books/[book_id] </li>
+    <li> /api/v1/category/list - returns the list of categories that exist in the database </li>
+    <li> /api/v1/category/details/[#] - returns the details of a category of id corresponding to the input # </li>
+    <li> /api/v1/books/list - returns the list of books that exist in the database </li>
+    <li> /api/v1/books/details/[#] - returns the details of the book of id corresponding to the input # </li>
+    <li> /api/v1/books/detailsbyuuid/[uuid] - returns the details of the book of uuid corresponding to the input # </li>
+    <li> /api/v1/books/categorybyid/[#] - returns the list of books that have the category of id corresponding to the input # </li>
+    <li> /api/v1/books/category/[name] - returns the list of books that have the category of name corresponding to the input name. The name has to be a complete match. </li>
+
 </ul>
